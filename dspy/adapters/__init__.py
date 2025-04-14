aace172a8835afe25d74b3235ddecd0e2a292bc6
Asif Uddin <uasif13@gmail.com>
2025-04-13 23:19:58 -0400

    before_fix

--- dspy/adapters/__init__.py
+++ dspy/adapters/__init__.py
@@ -1,12 +1,12 @@
-from dspy.adapters.base import Adapter
-from dspy.adapters.chat_adapter import ChatAdapter
-from dspy.adapters.json_adapter import JSONAdapter
-from dspy.adapters.types import Image, History
+# from dspy.adapters.base import Adapter
+# from dspy.adapters.chat_adapter import ChatAdapter
+# from dspy.adapters.json_adapter import JSONAdapter
+# from dspy.adapters.types import Image, History
 
-__all__ = [
-    "Adapter",
-    "ChatAdapter",
-    "JSONAdapter",
-    "Image",
-    "History",
-]
+# __all__ = [
+#     "Adapter",
+#     "ChatAdapter",
+#     "JSONAdapter",
+#     "Image",
+#     "History",
+# ]
